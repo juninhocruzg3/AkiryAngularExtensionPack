111264e10d1790dd2928073a6efec9eeb38e2090
António Cruz Júnior <antoniojr@icomp.ufam.edu.br>
2018-12-07 12:23:42 -0400

    initial commit

---
+++ Akiry Angular_Extensions_Pack_for_VS_Code.sh
@@ -0,0 +1,20 @@
+# Required
+code --install-extension alexiv.vscode-angular2-files           # provide a VS Code menu for create new components, services, ..
+code --install-extension Angular.ng-template                    # Official Angular Language Service: provide navigation between HTML and TS on tag click
+code --install-extension christian-kohler.path-intellisense     # provide path autocomplete
+code --install-extension eamodio.gitlens                        # provide git file and line history
+code --install-extension eg2.tslint                             # provide warnings for TS lint delicts :P rs
+code --install-extension robinbentley.sass-indented             # recognize SASS syntax and autocomplete
+code --install-extension glen-84.sass-lint                      # provide warnings for SASS lint delicts :P rs
+code --install-extension HookyQR.beautify                       # provide indentation for many languages
+code --install-extension infinity1207.angular2-switcher         # provide easily switcher among HTML, SASS, TS and Spec
+code --install-extension msjsdiag.debugger-for-chrome           # provide debugger on Chrome
+code --install-extension nsoult.typescript-imports-sort         # sort imports
+code --install-extension PeterJausovec.vscode-docker            # Docker - Official
+code --install-extension steoates.autoimport                    # provide autoimport for TS
+code --install-extension wayou.vscode-todo-highlight            # provide TODO and others highlight
+
+# Optional
+code --install-extension robertohuertasm.vscode-icons           # provide beautiful icons for VC Code
+code --install-extension mrmlnc.vscode-scss                     # provide variable recognization for SCSS only
+code --install-extension Zignd.html-css-class-completion        # provide class completion for CSS linked on only
